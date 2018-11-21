471657fa148aaa5847b59814771f7600d63c5bb3
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-21 19:17:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
   else
-    return true
+    true
   end
 end
